aefba2e7f919973729a1feab021dce483a7f02f1
David Masaryk <dadomasaryk@gmail.com>
2020-04-15 16:43:33 +0200

    Nova architektura

--- basecaller.py
+++ basecaller.py
@@ -24,15 +24,8 @@ import pickle
 
 np.random.seed(1)
 tf.set_random_seed(1)
-# tf.compat.v1.enable_eager_execution()
-
-# def plot_signal(signal):\
-#     X = [i for i in range(len(signal))]
-#     plt.plot(X, signal)
-#     plt.show()
-
 train_ds = read_raw_data_sets(r"train/", max_segments_num=100000)
-eval_ds = read_raw_data_sets(r"eval/", max_segments_num=2000)
+eval_ds = read_raw_data_sets(r"eval/", max_segments_num=20000)
 
 K.clear_session()
 
@@ -52,9 +45,57 @@ def prepare_data(batch_size=50, train=True):
         y = np.array(y_vals)
         y_lens = np.array(y_lens)
         yield [X, y, X_lens, y_lens], np.zeros(len(X))
+        
+        
+def create_CNN_network(features, labels, padding_value, units=256, output_dim=5, 
+                   dropout=0.2):
+    initializer = RandomNormal(mean=0.0, stddev=0.05, seed=1)
+    lstm_init = glorot_uniform(1)
+    input_data = Input(name='input', shape=(None, features))
+
+    # masking = Masking(mask_value=padding_value)(input_data)
+
+    pad = ZeroPadding1D(padding=(0, 310))(input_data)
+
+    cnn0 = Conv1D(filters=units, kernel_size=5, strides=1, activation='relu',
+               kernel_initializer=lstm_init, bias_initializer=initializer,
+               name='conv_1')(pad)
+    dropout0 = TimeDistributed(Dropout(dropout), name='dropout_1')(cnn0)
+
+    cnn1 = Conv1D(filters=units, kernel_size=5, strides=1, activation='relu',
+               kernel_initializer=lstm_init, bias_initializer=initializer,
+               name='conv_2')(dropout0)
+    dropout1 = TimeDistributed(Dropout(dropout), name='dropout_2')(cnn1)
+
+    cnn2 = Conv1D(filters=units, kernel_size=5, strides=2, activation='relu',
+               kernel_initializer=lstm_init, bias_initializer=initializer,
+               name='conv_3')(dropout1)
+    dropout2 = TimeDistributed(Dropout(dropout), name='dropout_3')(cnn2)
+
+    dense1 = TimeDistributed(Dense(units=units, kernel_initializer=initializer,
+                                   bias_initializer=initializer, activation='relu'), 
+                            name='fc_1')(dropout2)
+    dropout3 = TimeDistributed(Dropout(dropout), name='dropout_4')(dense1)
+
+    blstm0 = Bidirectional(LSTM(units, return_sequences=True, kernel_initializer=lstm_init, 
+                                dropout=dropout))(dropout3)
+    blstm1 = Bidirectional(LSTM(units, return_sequences=True, kernel_initializer=lstm_init, 
+                                dropout=dropout))(blstm0)
+    blstm2 = Bidirectional(LSTM(units, return_sequences=True, kernel_initializer=lstm_init, 
+                                dropout=dropout))(blstm1)
+    dense3 = TimeDistributed(Dense(units=units, kernel_initializer=initializer,
+                                   bias_initializer=initializer,activation='relu'),
+                                   name='fc_3')(blstm2)
+    dropout4 = TimeDistributed(Dropout(dropout), name='dropout_5')(dense3)
+
+    dense4 = TimeDistributed(Dense(labels + 1, name="dense"))(dropout4)
+    y_pred = Activation('softmax', name='softmax')(dense4)
+    network = CTCModel([input_data], [y_pred])
+    network.compile(Nadam())
+    return network
 
 
-def create_network(features, labels, padding_value, units=256, output_dim=5, 
+def create_LSTM_network(features, labels, padding_value, units=256, output_dim=5, 
                    dropout=0.2):
     initializer = RandomNormal(mean=0.0, stddev=0.05, seed=1)
     lstm_init = glorot_uniform(1)
@@ -79,8 +120,6 @@ def create_network(features, labels, padding_value, units=256, output_dim=5,
                                 dropout=dropout))(blstm0)
     blstm2 = Bidirectional(LSTM(units, return_sequences=True, kernel_initializer=lstm_init, 
                                 dropout=dropout))(blstm1)
-#     blstm3 = Bidirectional(LSTM(units, return_sequences=True, dropout=dropout))(blstm2)
-#     dropout2 = Bidirectional(LSTM(units, return_sequences=True, dropout=dropout))(blstm3)
 
     dense3 = TimeDistributed(Dense(units=units, kernel_initializer=initializer,
                                    bias_initializer=initializer,activation='relu'),
@@ -101,9 +140,9 @@ features = 1
 padding_value = 100
 
 K.clear_session()
-network = create_network(features, labels, padding_value, units=300)
+network = create_CNN_network(features, labels, padding_value, units=300)
 print(network.summary())
-X_val, y_val = prepare_data(2500, train=False).__next__()
+X_val, y_val = prepare_data(20000, train=False).__next__()
 
 es = EarlyStopping(monitor='val_loss', mode='min', patience=5, restore_best_weights=True)
 lr_cb = ReduceLROnPlateau(factor=0.2, patience=2, verbose=0, min_lr=0.0001)
@@ -111,9 +150,9 @@ history = network.fit_generator(prepare_data(batch_size), steps_per_epoch=1000,
                                 epochs=epochs, validation_data=(X_val, y_val), 
                                 callbacks=[es, lr_cb])
 
-with open('history.pickle', 'wb') as handle:
+with open('CNN_history.pickle', 'wb') as handle:
     pickle.dump(history, handle, protocol=pickle.HIGHEST_PROTOCOL)
-network.save_model('models/')
+network.save_model('CNN_model/')
 
 pred = network.predict([X_val[0], X_val[2]], batch_size=batch_size, 
                         max_value=padding_value)
@@ -123,10 +162,11 @@ with open('X_val.pickle', 'wb') as handle:
     pickle.dump(X_val, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 try:
-    network.model_train.save_weights('models/my_model_weights.h5')
+    network.model_train.save_weights('CNN_model/CNN_weights.h5')
 except:
     pass
 
+from Bio.pairwise2 import align
 y_val_true = []
 for y_valid, lenght in zip(X_val[1], X_val[3]):
     y_val_true.append(y_valid[:lenght])
@@ -136,13 +176,16 @@ for y_true, y_pred in zip(y_val_true, pred):
     y_true = ''.join(y_true.astype(str))
     y_pred = ''.join(y_pred.astype('int64').astype(str))
     y_pred = y_pred.replace('-1', '')
+    if not len(y_pred):
+        accuracies.append(0)
+        continue
     aligned = np.array(align.globalxx(y_true, y_pred))[:, [2, -1]].astype('float16')
     align_score_ix = aligned[:, 0].argmax()
     align_score = aligned[:, 0][align_score_ix]
     align_length = aligned[:, 1][align_score_ix]
     accuracies.append(align_score/align_length)
 
-with open('accuracies.pickle', 'wb') as handle:
+with open('CNN_accuracies.pickle', 'wb') as handle:
     pickle.dump(accuracies, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
 print(np.mean(accuracies))
